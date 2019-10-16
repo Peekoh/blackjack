42184be7682b44b914e44b4c812b8874fdfd7556
Dana Hedrick <dana.marie.hedrick@gmail.com>
2019-10-16 13:57:57 -0700

    changed namespace on card and deck + wrote otu player.cs

--- Person.cs
+++ Person.cs
@@ -4,4 +4,6 @@ abstract class Person
     public int HandValue{get; set;}
     private bool isTurn;
     public bool IsTurn{get; set;}
+    private bool isGame;
+    public bool IsGame{get; set;}
 }
